71089da820f0898747a19064f6414fd2b14e8aa8
elizavetaArt <elizavetashut1997@gmail.com>
2018-10-15 22:16:40 +0300

    Statistics two

--- js/index.js
+++ js/index.js
@@ -3,18 +3,17 @@ let CLOUD_HEIGHT  = 200;
 let CLOUD_X = 100;
 let CLOUD_Y = 50;
 let GAP = 10;
-let FONT_GAP = 15;
-let TEXT_WIDTH = 50;
-let BAR_HEIGHT = 20;
-let barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+let FONT_GAP = 70;
+let TEXT_HEIGHT = 120;
+
 let renderCloud = function(ctx, x, y, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 let getMaxElement = function(arr) {
     let maxElement = arr[0];
-    for( let i = 1; i < arr.length; i++) {
-        if (arr[i] > maxElement) {
+    for (let i = 1; i < arr.length; i++) {
+        if(maxElement < arr[i]) {
             maxElement = arr[i];
         }
     }
@@ -24,11 +23,10 @@ window.renderStatistics = function (ctx, players, times) {
     renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
     renderCloud(ctx, CLOUD_X, CLOUD_Y, '#a9ffa3');
 
-    ctx.fillStyle = '#000';
-    let maxTime = getMaxElement(times);
-
-    for(let i = 0; i < players.length; i++) {
-        // ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
-        ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+    for (let i = 0; i < players.length; i++) {
+        ctx.fillStyle = '#94acff';
+        let maxTime = getMaxElement(times);
+        ctx.fillText(players[i], TEXT_HEIGHT + (CLOUD_X * i), 2 * (CLOUD_X + GAP * 2));
+        ctx.fillRect(TEXT_HEIGHT + (CLOUD_X * i), FONT_GAP, FONT_GAP + GAP, (CLOUD_X + CLOUD_Y) * times[i] / maxTime);
     }
 };
