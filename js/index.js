822ea80d9c90479d40b9d39c1a436ba0c9380794
elizavetaArt <elizavetashut1997@gmail.com>
2018-10-16 19:18:15 +0300

    best statistic for game

--- js/index.js
+++ js/index.js
@@ -3,30 +3,34 @@ let CLOUD_HEIGHT  = 200;
 let CLOUD_X = 100;
 let CLOUD_Y = 50;
 let GAP = 10;
-let FONT_GAP = 70;
-let TEXT_HEIGHT = 120;
-
+let FONT_GAP = 15;
+let TEXT_WIDTH = 60;
+let BAR_HEIGHT = 30;
+let barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
 let renderCloud = function(ctx, x, y, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 let getMaxElement = function(arr) {
     let maxElement = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if(maxElement < arr[i]) {
+    for( let i = 1; i < arr.length; i++) {
+        if (arr[i] > maxElement) {
             maxElement = arr[i];
         }
     }
     return maxElement;
 };
+let getRandomColor = function() {
+    let color = Math.floor(Math.random() * 16777216).toString(16);
+    return "#" + color;
+};
 window.renderStatistics = function (ctx, players, times) {
     renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
-    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#a9ffa3');
-
-    for (let i = 0; i < players.length; i++) {
-        ctx.fillStyle = '#94acff';
-        let maxTime = getMaxElement(times);
-        ctx.fillText(players[i], TEXT_HEIGHT + (CLOUD_X * i), 2 * (CLOUD_X + GAP * 2));
-        ctx.fillRect(TEXT_HEIGHT + (CLOUD_X * i), FONT_GAP, FONT_GAP + GAP, (CLOUD_X + CLOUD_Y) * times[i] / maxTime);
+    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#faffff');
+    let maxTime = getMaxElement(times);
+    for(let i = 0; i < players.length; i++) {
+        ctx.fillStyle = getRandomColor();
+        ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
+        ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
     }
 };
